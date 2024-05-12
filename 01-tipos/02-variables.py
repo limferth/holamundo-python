734c7ee000d1aa3efcdefbe4e99ec84cd7445b6d
limferth <limferth@gmail.com>
2024-05-12 17:03:41 -0400

    primer commit de la clase

---
+++ 01-tipos/02-variables.py
@@ -0,0 +1,9 @@
+# 
+
+nombre_curso = "ultimate python"
+print(nombre_curso);
+
+alumnos - 5000
+puntaje = 9.9
+publicado = True
+
---
+++ 01-tipos/03-strings.py
@@ -0,0 +1,19 @@
+# clase dde los strings
+
+nombre_curso = 'ultimate python';
+descripcion_curso = """
+ultimate python
+este curso contempla todos lo que 
+necesitas para traabajr 
+"""
+print(nombre_curso, descripcion_curso)
+
+print(len(nombre_curso));
+print(nombre_curso[0]);
+print (nombre_curso[0:8]);
+print (nombre_curso[9:]); # como no le pasaste absolutamente nada entonces yo relleno todo.
+
+print(nombre_curso[:8]) # conmo no le pasamos python asume desde el principio osea 0.
+
+
+
